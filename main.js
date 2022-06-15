158a1e6b9f2997aee68d6b83d35a614a8b741210
PapiManu <45321589+PapiManu@users.noreply.github.com>
2022-06-15 14:28:06 +0200

    Finalisation des règle js pour la page d'accueil

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 home_page_button = document.getElementById('home_page_button');
 accueil = document.getElementById('accueil');
 header = document.getElementById('header');
+select_page = document.getElementById('select_page');
 
 home_page_button.addEventListener('click', () => {
-    accueil.classList.remove('accueil_open');
-    accueil.classList.add('accueil_close')
     header.classList.remove('header_apparition');
     header.classList.add('header_disparition');
+    accueil.classList.remove('accueil_open');
+    accueil.classList.add('accueil_close')
+    select_page.classList.add('select_page_apparition');
 })
