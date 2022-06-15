5e15dc895ec554ac7413e6a31426a950d4844cbb
PapiManu <45321589+PapiManu@users.noreply.github.com>
2022-06-15 15:00:36 +0200

    Ajout d'une fonction pour le displaynone

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-home_page_button = document.getElementById('home_page_button');
+homePageButton = document.getElementById('home_page_button');
 accueil = document.getElementById('accueil');
 header = document.getElementById('header');
-select_page = document.getElementById('select_page');
+selectPage = document.getElementById('select_page');
 
-home_page_button.addEventListener('click', () => {
+homePageButton.addEventListener('click', () => {
     header.classList.remove('header_apparition');
     header.classList.add('header_disparition');
     accueil.classList.remove('accueil_open');
     accueil.classList.add('accueil_close')
-    select_page.classList.add('select_page_apparition');
+    selectPage.classList.add('select_page_apparition');
+    setTimeout(displayNone, 1500, accueil)
 })
+
+function displayNone(appletName) {
+    appletName.classList.add("display_none")
+}
+
