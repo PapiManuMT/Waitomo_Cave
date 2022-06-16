f05422d14d7ef070b12ef590e9a23979606badee
PapiManu <45321589+PapiManu@users.noreply.github.com>
2022-06-16 16:47:19 +0200

    Abandon de la class display_none

--- main.js
+++ main.js
@@ -3,16 +3,22 @@ accueil = document.getElementById('accueil');
 header = document.getElementById('header');
 selectPage = document.getElementById('select_page');
 
+function start() {
+    selectPage.style.display="none";
+}
+start()
+
 homePageButton.addEventListener('click', () => {
-    // header.classList.remove('header_apparition');
-    // header.classList.add('header_disparition');
     accueil.classList.remove('accueil_open');
     accueil.classList.add('accueil_close')
     selectPage.classList.add('select_page_apparition');
     setTimeout(displayNone, 1500, accueil)
-    setTimeout(displayNone, 1500, header)   
+    setTimeout(displayFlex, 1500, selectPage)
 })
 
 function displayNone(appletName) {
     appletName.classList.add("display_none")
+}
+function displayFlex(appletName) {
+    appletName.style.display="flex";
 }
